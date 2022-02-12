861da55031f7342e42fa164f0c332cbf26886b2d
servisofts <69025139+servisofts@users.noreply.github.com>
2022-02-12 19:41:20 -0400

    Ya conecte

--- capp/capp/Program.cs
+++ capp/capp/Program.cs
@@ -8,16 +8,19 @@ namespace capp
     {
         static void Main(string[] args)
         {
-        
+
             CZKEMClass axCZKEM1 = new CZKEMClass();
             String ip = "192.168.1.201";
-            int port = 10;
+            int port = 4370;
             String commKey = "0";
             int idwErrorCode = 0;
 
-            axCZKEM1.SetCommPassword(Convert.ToInt32(commKey));
-
-            axCZKEM1.Connect_Net(ip, Convert.ToInt32(port));
+            //axCZKEM1.SetCommPassword(Convert.ToInt32(commKey));
+            Console.WriteLine($"Intentando conectar a {ip}");
+            if (axCZKEM1.Connect_Net(ip, Convert.ToInt32(port)) == true) {
+                Console.WriteLine("Conexion exitosa");
+            }
+            Console.WriteLine("Fin");
 
         }
     }
