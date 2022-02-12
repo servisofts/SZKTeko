7bc81213a514e1732137027186119fe363df82b4
servisofts <69025139+servisofts@users.noreply.github.com>
2022-02-12 19:36:59 -0400

    commit

--- capp/capp/Program.cs
+++ capp/capp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
+using zkemkeeper;
+
 namespace capp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
+            CZKEMClass axCZKEM1 = new CZKEMClass();
+            String ip = "192.168.1.201";
+            int port = 10;
+            String commKey = "0";
+            int idwErrorCode = 0;
+
+            axCZKEM1.SetCommPassword(Convert.ToInt32(commKey));
+
+            axCZKEM1.Connect_Net(ip, Convert.ToInt32(port));
+
         }
     }
 }
