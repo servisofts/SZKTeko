555249f289203dfd11ae1daae83275b55f903687
servisofts <69025139+servisofts@users.noreply.github.com>
2022-02-13 02:30:40 -0400

    chambios

---
+++ capp/capp/src/SSocket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace capp
+{
+    internal class SSocket
+    {
+        private String ip;
+        private int port;
+        private TcpClient socket;
+        private NetworkStream stream;
+        private bool estado;
+        public SSocket(String ip, int port) { 
+            this.ip = ip;
+            this.port = port;
+            this.Connectar();
+        }
+
+        public void Connectar() {
+              this.socket = new TcpClient();
+             this.socket.Connect(this.ip, this.port);
+            Console.WriteLine($"Session Iniciada");
+            this.estado = true;
+            this.stream = this.socket.GetStream();
+            Thread t1 = new Thread(new ThreadStart(this.Receive));
+            t1.Start();
+        }
+
+        public void Send(String msn) { 
+            
+            byte[] data = Encoding.UTF8.GetBytes(msn+"\n");
+            this.stream.Write(data, 0, data.Length);
+            this.stream.Flush();
+        }
+        public void onClose()
+        {
+            Console.WriteLine($"Session closed");
+            this.estado = false;
+        }
+        public void Receive()
+        {
+            while (this.estado == true) {
+                Byte[] bytesReceived = new Byte[(int)this.socket.ReceiveBufferSize];
+                this.stream.Read(bytesReceived, 0, (int)this.socket.ReceiveBufferSize);
+                String data = Encoding.UTF8.GetString(bytesReceived);
+                if (data.Length <=0 ) {
+                    this.estado = false;
+                    this.onClose();
+                    return;
+                }
+                Console.WriteLine($"SERVER : {data}");
+            }
+            //int bytes= this.socket.Receive(bytesReceived);
+
+        }
+    }
+}
--- capp/capp/src/SZK.cs
+++ capp/capp/src/SZK.cs
@@ -12,12 +12,13 @@ namespace capp
             
         private String ip;
         private int port;
-        private CZKEMClass CZKE;
+        private CZKEM CZKE;
+        private int MachineNumber = 1;
         public SZK(String ip, int port)
         {
             this.ip = ip;
             this.port = port;
-            this.CZKE = new CZKEMClass();
+            this.CZKE = new CZKEM();
         }
 
         public Boolean connectar() {
@@ -31,19 +32,81 @@ namespace capp
             this.log("Conexion fallida");
             return false;
         }
+        public void setUser()
+        {
 
+            this.CZKE.EnableDevice(1, false);//disable the device
+            if (this.CZKE.SSR_SetUserInfo(this.MachineNumber, "1", "Juan", "", 1, true) == true)
+            {
+                this.log("Insertado");
+            }
+            else
+            {
+                this.error();
+            }
+            this.CZKE.RefreshData(this.MachineNumber);
+            this.CZKE.EnableDevice(1, true);//enable the device
+
+        }
         public void getDeviceInfo()
         {
             String mac = "";
-            this.CZKE.GetDeviceMAC(0, ref mac);
+            this.CZKE.GetDeviceMAC(1, ref mac);
             this.log($"{mac}");
-            this.log($"{mac}");
+
+
+            this.CZKE.EnableDevice(1, true);
+            int number = 0;
+            int mn = 0;
+            int bn = 0;
+            int dmp = 0;
+            int enable=1;
+
+            this.CZKE.GetAllUserID(1, ref number, ref mn, ref bn, ref dmp, ref enable);
+
+            this.log($"Roles:  {number}");
+            
         }
 
+        public int getCapacityInfo()
+        {
+            int ret = 0;
+
+            int adminCnt = 0;
+            int userCount = 0;
+            int fpCnt = 0;
+            int recordCnt = 0;
+            int pwdCnt = 0;
+            int oplogCnt = 0;
+            int faceCnt = 0;
+
+       
+            this.CZKE.EnableDevice(1, false);//disable the device
+
+            this.CZKE.GetDeviceStatus(1, 2, ref userCount);
+            this.CZKE.GetDeviceStatus(1, 1, ref adminCnt);
+            this.CZKE.GetDeviceStatus(1, 3, ref fpCnt);
+            this.CZKE.GetDeviceStatus(1, 4, ref pwdCnt);
+            this.CZKE.GetDeviceStatus(1, 5, ref oplogCnt);
+            this.CZKE.GetDeviceStatus(1, 6, ref recordCnt);
+            this.CZKE.GetDeviceStatus(1, 21, ref faceCnt);
+
+            this.CZKE.EnableDevice(1, true);//enable the device
+
+            this.log($" adminCnt: {adminCnt}");
+            this.log($" userCount: {userCount}");
+
+            return ret;
+        }
 
         private void log(String mensaje) {
                Console.WriteLine(mensaje);
         }
-
+        private void error()
+        {
+            int error = 0;
+            this.CZKE.GetLastError(ref error);
+            Console.WriteLine($"Error Code={error}");
+        }
     }
 }
