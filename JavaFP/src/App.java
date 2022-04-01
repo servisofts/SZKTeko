817603909f5befbdcf485851d34ea9d357435e38
servisofts <69025139+servisofts@users.noreply.github.com>
2022-03-31 23:34:47 -0400

    commit

--- JavaFP/src/App.java
+++ JavaFP/src/App.java
@@ -1,5 +1,7 @@
+import Vista.InicioFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new InicioFrame();
     }
 }
