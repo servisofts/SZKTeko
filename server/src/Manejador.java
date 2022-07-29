c3ae39529ff51ea8437cb599b36806586762bf07
Ricardo Paz Demiquel <rickypazd@icloud.com>
2022-07-29 00:45:26 -0400

    add

--- server/src/Manejador.java
+++ server/src/Manejador.java
@@ -14,7 +14,9 @@ import Server.SSSAbstract.SSSessionAbstract;
 public class Manejador {
     public static void onMessage(JSONObject obj, SSSessionAbstract session) {
         if (session != null) {
-            SConsole.log(session.getIdSession(), "\t|\t", obj.getString("component"), obj.getString("type"));
+            if (!obj.getString("type").equals("ping")) {
+                SConsole.log(session.getIdSession(), "\t|\t", obj.getString("component"), obj.getString("type"));
+            }
         }
         if (obj.isNull("component")) {
             return;
